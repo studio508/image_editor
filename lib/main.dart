f0fb78f0429059adffd9656d7a521cb62f0f5734
studio508 <jy.studio508@gmail.com>
2023-06-25 17:44:02 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:image_editor/screen/home_screen.dart';
+import 'dart:io';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
